fc385e9e0705ac5e9a300d26fa794182507bfaf0
Gilberto Vargas <ticox.vargas@gmail.com>
2016-11-14 23:11:56 -0400

    Hice el agregar representantes y el agregar alumnnos vinculandolos de una con el representante.

--- fumict.sql
+++ fumict.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
--- Servidor: localhost
--- Tiempo de generación: 15-11-2016 a las 00:56:34
--- Versión del servidor: 10.1.13-MariaDB
--- Versión de PHP: 5.5.37
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-11-2016 a las 04:10:31
+-- Versión del servidor: 5.5.39
+-- Versión de PHP: 5.4.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de datos: `fumict`
@@ -23,23 +23,43 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `compra`
+--
+
+CREATE TABLE IF NOT EXISTS `compra` (
+`id_compra` int(11) NOT NULL,
+  `id_representante` int(11) NOT NULL,
+  `id_representado` int(11) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `compra`
+--
+
+INSERT INTO `compra` (`id_compra`, `id_representante`, `id_representado`) VALUES
+(1, 1, 1),
+(2, 1, 2);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `menu`
 --
 
-CREATE TABLE `menu` (
-  `id_menu` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `menu` (
+`id_menu` int(11) NOT NULL,
   `titulo` varchar(50) NOT NULL,
   `enlace` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
 -- Volcado de datos para la tabla `menu`
 --
 
 INSERT INTO `menu` (`id_menu`, `titulo`, `enlace`) VALUES
-(4, 'ALUMNOS', 'alumnos'),
-(6, 'TEMAS', 'tema'),
-(7, 'OBJETIVOS', 'objetivos');
+(9, 'Agregar Representante', 'registrar'),
+(10, 'Agregar Alumnos', 'alumno'),
+(11, 'Administrar', 'administrar');
 
 -- --------------------------------------------------------
 
@@ -47,12 +67,67 @@ INSERT INTO `menu` (`id_menu`, `titulo`, `enlace`) VALUES
 -- Estructura de tabla para la tabla `permisos`
 --
 
-CREATE TABLE `permisos` (
-  `id_permisos` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `permisos` (
+`id_permisos` int(11) NOT NULL,
   `id_menu` int(11) NOT NULL,
   `id_role` int(11) NOT NULL,
   `permiso` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Volcado de datos para la tabla `permisos`
+--
+
+INSERT INTO `permisos` (`id_permisos`, `id_menu`, `id_role`, `permiso`) VALUES
+(3, 9, 2, 1),
+(4, 10, 2, 1),
+(5, 11, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `representados`
+--
+
+CREATE TABLE IF NOT EXISTS `representados` (
+`id_representado` int(11) NOT NULL,
+  `nombre` varchar(300) COLLATE utf8_bin NOT NULL,
+  `apellidos` varchar(300) COLLATE utf8_bin NOT NULL,
+  `grado` varchar(300) COLLATE utf8_bin NOT NULL,
+  `talla_camisa` varchar(300) COLLATE utf8_bin NOT NULL,
+  `talla_pantalon` varchar(300) COLLATE utf8_bin NOT NULL,
+  `id_representante` int(11) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=13 ;
+
+--
+-- Volcado de datos para la tabla `representados`
+--
+
+INSERT INTO `representados` (`id_representado`, `nombre`, `apellidos`, `grado`, `talla_camisa`, `talla_pantalon`, `id_representante`) VALUES
+(1, 'tico', 'vargas', '6to', '12', '32', 1),
+(2, 'mali', 'vargas', '3ro', '12', '28', 1),
+(12, 'asdasd', 'adsad', '6', '12', '12', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `representantes`
+--
+
+CREATE TABLE IF NOT EXISTS `representantes` (
+`id_representante` int(11) NOT NULL,
+  `cedula` int(11) NOT NULL,
+  `nombres` varchar(200) COLLATE utf8_bin NOT NULL,
+  `apellidos` varchar(200) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `representantes`
+--
+
+INSERT INTO `representantes` (`id_representante`, `cedula`, `nombres`, `apellidos`) VALUES
+(1, 23347026, 'Gilberto ', 'Vargas'),
+(2, 23347025, 'tico', 'vargas');
 
 -- --------------------------------------------------------
 
@@ -60,22 +135,19 @@ CREATE TABLE `permisos` (
 -- Estructura de tabla para la tabla `role`
 --
 
-CREATE TABLE `role` (
-  `id_role` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `role` (
+`id_role` int(11) NOT NULL,
   `role` varchar(50) NOT NULL,
   `peso` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Volcado de datos para la tabla `role`
 --
 
 INSERT INTO `role` (`id_role`, `role`, `peso`) VALUES
-(1, 'admin_sistem', 1),
-(2, 'jefe', 2),
-(3, 'asistente', 3),
-(4, 'anonimo', 4),
-(5, 'general', 5);
+(1, 'admin', 1),
+(2, 'anonimo', 1);
 
 -- --------------------------------------------------------
 
@@ -83,91 +155,122 @@ INSERT INTO `role` (`id_role`, `role`, `peso`) VALUES
 -- Estructura de tabla para la tabla `usuario`
 --
 
-CREATE TABLE `usuario` (
-  `id_usuario` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `usuario` (
+`id_usuario` int(11) NOT NULL,
   `id_role` int(11) NOT NULL DEFAULT '2',
   `login` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
   `password` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `estado` int(11) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=2 ;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
 INSERT INTO `usuario` (`id_usuario`, `id_role`, `login`, `password`, `estado`) VALUES
-(1, 1, 'admin', '53362d5ea52a28e1a960323ea19b02cb2b828026', 1),
-(2, 2, 'juan', '53362d5ea52a28e1a960323ea19b02cb2b828026', 1),
-(3, 3, 'jose', '53362d5ea52a28e1a960323ea19b02cb2b828026', 1),
-(4, 5, 'jesus', '53362d5ea52a28e1a960323ea19b02cb2b828026', 1);
+(1, 1, 'admin', 'eeb3980e639031f9c0906cd52e2239094e9ee612', 1);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `compra`
+--
+ALTER TABLE `compra`
+ ADD PRIMARY KEY (`id_compra`), ADD KEY `id_representado` (`id_representado`), ADD KEY `id_representante` (`id_representante`);
+
+--
 -- Indices de la tabla `menu`
 --
 ALTER TABLE `menu`
-  ADD PRIMARY KEY (`id_menu`);
+ ADD PRIMARY KEY (`id_menu`);
 
 --
 -- Indices de la tabla `permisos`
 --
 ALTER TABLE `permisos`
-  ADD PRIMARY KEY (`id_permisos`),
-  ADD KEY `id_menu` (`id_menu`,`id_role`),
-  ADD KEY `id_role` (`id_role`);
+ ADD PRIMARY KEY (`id_permisos`), ADD KEY `id_menu` (`id_menu`,`id_role`), ADD KEY `id_role` (`id_role`);
+
+--
+-- Indices de la tabla `representados`
+--
+ALTER TABLE `representados`
+ ADD PRIMARY KEY (`id_representado`), ADD KEY `id_representante` (`id_representante`);
+
+--
+-- Indices de la tabla `representantes`
+--
+ALTER TABLE `representantes`
+ ADD PRIMARY KEY (`id_representante`);
 
 --
 -- Indices de la tabla `role`
 --
 ALTER TABLE `role`
-  ADD PRIMARY KEY (`id_role`);
+ ADD PRIMARY KEY (`id_role`);
 
 --
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuario`),
-  ADD KEY `id_role_2` (`id_role`),
-  ADD KEY `id_role_3` (`id_role`),
-  ADD KEY `id_role_4` (`id_role`),
-  ADD KEY `id_role_5` (`id_role`);
+ ADD PRIMARY KEY (`id_usuario`), ADD KEY `id_role_2` (`id_role`), ADD KEY `id_role_3` (`id_role`), ADD KEY `id_role_4` (`id_role`), ADD KEY `id_role_5` (`id_role`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `compra`
+--
+ALTER TABLE `compra`
+MODIFY `id_compra` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT de la tabla `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT de la tabla `permisos`
 --
 ALTER TABLE `permisos`
-  MODIFY `id_permisos` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id_permisos` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT de la tabla `representados`
+--
+ALTER TABLE `representados`
+MODIFY `id_representado` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT de la tabla `representantes`
+--
+ALTER TABLE `representantes`
+MODIFY `id_representante` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `role`
 --
 ALTER TABLE `role`
-  MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `usuario`
+-- Filtros para la tabla `compra`
 --
-ALTER TABLE `usuario`
-  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `venta`.`role` (`id_role`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `compra`
+ADD CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`id_representante`) REFERENCES `representantes` (`id_representante`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `compra_ibfk_2` FOREIGN KEY (`id_representado`) REFERENCES `representados` (`id_representado`);
+
+--
+-- Filtros para la tabla `representados`
+--
+ALTER TABLE `representados`
+ADD CONSTRAINT `representados_ibfk_1` FOREIGN KEY (`id_representante`) REFERENCES `representantes` (`id_representante`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- site/controllers/principalController.php
+++ site/controllers/principalController.php
@@ -22,7 +22,22 @@ class principalController extends Controller
 			$this->_view->renderizar('index');
 							
 			
-	}	
+	}
+
+
+	public function registrar()
+    {
+
+    	 $array = array();
+       
+		
+			$this->_view->setJs(array('index'));
+			$this->_view->setCss(array('css'));
+        	$this->_view->titulo = 'index';
+			$this->_view->renderizar('index');
+							
+			
+	}		
 	
 }
 
